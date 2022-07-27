ecc5b22437e34c7163884293d0ab77a55cf35f7a
Marco Arriaga <marriaga158@gmail.com>
2022-07-26 22:43:18 -0400

    feat: create deletecommands

---
+++ deletecommand.js
@@ -0,0 +1,39 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const { clientId, token } = require('./config.json');
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+let global;
+let guildId;
+
+// I'm going to abstract this so I don't have to reuse this all the time
+function getArg(num, name) {
+	if (num >= process.argv.length) throw `Error: argument ${num} ${name} not provided.`;
+	return process.argv[num];
+}
+
+// flag | commandId | guildId (optional)
+
+const commandScope = getArg(2, 'commandScope');
+if (commandScope.toLowerCase === 'gu') {
+	global = false;
+	guildId = getArg(4, 'guildId');
+} else if (commandScope.toLowerCase === 'gl') {
+	global = true;
+}
+
+const commandId = getArg(3, 'commandId');
+guildId = getArg(4, 'guildId');
+
+if (global) {
+	// for global commands
+	rest.delete(Routes.applicationCommand(clientId, commandId))
+		.then(() => console.log('Successfully deleted application command'))
+		.catch(console.error);
+} else {
+	// for guild-based commands
+	rest.delete(Routes.applicationGuildCommand(clientId, guildId, commandId))
+		.then(() => console.log('Successfully deleted guild command'))
+		.catch(console.error);
+}
